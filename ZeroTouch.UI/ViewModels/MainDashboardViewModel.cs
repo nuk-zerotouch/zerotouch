d57fe1c36cd6035c87470a4532a4304ae100233d
mellivorandy <mellivorandy@gmail.com>
2025-11-13 08:55:36 +0800

    feat(UI): add temperature details

--- ZeroTouch.UI/ViewModels/MainDashboardViewModel.cs
+++ ZeroTouch.UI/ViewModels/MainDashboardViewModel.cs
@@ -10,9 +10,12 @@ namespace ZeroTouch.UI.ViewModels
 
         [ObservableProperty] private string _location = "高雄市";
         [ObservableProperty] private string _weatherCondition = "Loading...";
-        [ObservableProperty] private string _temperature = "--°C";
         [ObservableProperty] private string _pop = "--%";
         [ObservableProperty] private string _comfort = "Loading...";
+        [ObservableProperty] private string _weatherIconPath = "avares://ZeroTouch.UI/Assets/Icons/Dark/rainy.gif";
+        [ObservableProperty] private string _minTemperature = "--°";
+        [ObservableProperty] private string _maxTemperature = "--°";
+
 
         public MainDashboardViewModel()
         {
@@ -21,11 +24,23 @@ namespace ZeroTouch.UI.ViewModels
 
         private async Task LoadWeatherAsync()
         {
-            var (condition, temp, pop, comfort) = await _weatherService.GetWeatherAsync(Location);
+            var (condition, minT, maxT, pop, comfort) = await _weatherService.GetWeatherAsync(Location);
             WeatherCondition = condition;
-            Temperature = temp;
+            MinTemperature = $"{minT}°";
+            MaxTemperature = $"{maxT}°";
             Pop = pop;
             Comfort = comfort;
+        }
+
+        private string GetIconPath(string condition)
+        {
+            if (condition.Contains("晴"))
+                return "avares://ZeroTouch.UI/Assets/Icons/Dark/sunny.svg";
+            if (condition.Contains("雨"))
+                return "avares://ZeroTouch.UI/Assets/Icons/Dark/rainy-1.svg";
+            if (condition.Contains("陰"))
+                return "avares://ZeroTouch.UI/Assets/Icons/Dark/cloudy.svg";
+            return "avares://ZeroTouch.UI/Assets/Icons/Dark/rainy.gif";
         }
     }
 }
